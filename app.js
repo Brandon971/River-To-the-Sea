904d1c6b51b9045bfaf4cb9fe44ef73a5e445b72
Succeed Group <r.swinton971@gmail.com>
2023-12-12 13:57:18 +0200

    music button

--- app.js
+++ app.js
@@ -23,3 +23,19 @@ window.onmousemove = function(event) {
     div.style.top = divy + -y + "px";
 }
 /* Freedom For Palestine For Ever */
+
+
+function toggleAudio() {
+    var audioElement = document.getElementById('player')
+    var soundOn = document.getElementById('play')
+    var soundOff = document.getElementById('pause')
+    if (audioElement.paused) {
+      audioElement.play();
+      $(soundOn).show();
+      $(soundOff).hide();
+    } else {
+      audioElement.pause();
+      $(soundOn).hide();
+      $(soundOff).show();
+    }
+  } 
